3272a95a1220609de127178cbd95fff2e9cabe5e
mazjin <mazjin55555@gmail.com>
2015-08-04 11:13:14 +0100

    finish ranking system and add german dictionary for additional ranking options, challenge 223e complete

--- challenge223e.java
+++ challenge223e.java
@@ -5,7 +5,7 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 
 public class challenge223e{
-  public int garland(String wordIn) {
+  public static int garland(String wordIn) {
     String startChars="";
     String endChars="";
     String matchChars="";
@@ -14,23 +14,15 @@ public class challenge223e{
       for (i=1; i<wordIn.length(); i++){
           startChars=wordIn.substring(0,i);
           endChars=wordIn.substring((wordIn.length()-i),(wordIn.length()));
-          System.out.println(startChars+","+ endChars);
           if (startChars.equals(endChars)){
             matchChars=startChars;
             degree=startChars.length();
             break;
           }
       }
-      //if (degree==0){
-        //System.out.println("No matches found, "+wordIn+" must not be a Garland word.");
-      //}
-      //else {
-        //System.out.println("'" +wordIn+ "' is a Garland word of degree " + degree +", (starts and ends with "+ matchChars+")");
-        //garlandChain(wordIn,degree);
-      //}
-      return degree
+      return degree;
   }
-  public static void main(String[] args){
+  public static void main(String[] args) throws IOException{
     System.out.println("Welcome to the Garland word checker.");
     Scanner in=new Scanner(System.in);
     String checkWord;
@@ -47,12 +39,12 @@ public class challenge223e{
         break;
       }
       else {
-        garland(checkWord);
+        System.out.println("Word " + checkWord+ "has degree " + challenge223e.garland(checkWord));
       }
     }while (checkWord!="");
 
   }
-  public String garlandChain(String wordIn, int degree){
+  public static String garlandChain(String wordIn, int degree){
     Scanner in=new Scanner(System.in);
     do {
       System.out.println("Please enter the desired length of the chain as an integer.");
@@ -68,29 +60,30 @@ public class challenge223e{
         chain=chain+wordIn.substring(degree,wordIn.length());
       }
     }
-    return chain
+    return chain;
   }
   public static void rankings() throws IOException{
-    FileReader fr = new FileReader("223e-enable1.txt");
+    FileReader fr = new FileReader("german.dic");
     BufferedReader textReader = new BufferedReader(fr);
-    //TODO: finish implementing filereading and use to generate rankings
     String line;
-    String largestWords;
+    String largestWords="";
     int largestDegree=0;
     do {
-      line=textReader.readLine();
-      switch (garland(line)){
-        if garland(line)>largestDegree\
+        line=textReader.readLine();
+        if (line==null){
+          break;
+        }
+        if (challenge223e.garland(line)>largestDegree)
         {
           largestDegree=garland(line);
           largestWords=line;
+          System.out.println(largestWords +" = " +largestDegree);
         }
-        else if (garland(line)=largestDegree)
+        else if (challenge223e.garland(line)==largestDegree)
         {
-          largestWords=largestWords + //////// + line;
+          largestWords=largestWords + " , " + line;
         }
-      }
-    } while (line.isEmpty());
-
+      }while (line!=null);
+    System.out.println("The largest garland word(s), with a degree of "+ largestDegree+": "+ largestWords);
   }
 }
