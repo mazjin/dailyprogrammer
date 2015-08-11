a93f5384eef2de813e9167d9fa689ff791120fd5
mazjin <mazjin55555@gmail.com>
2015-08-11 11:49:00 +0100

    comment code for readability, dummy out test code

--- c221e.java
+++ c221e.java
@@ -3,8 +3,8 @@ import java.util.Random;
 import java.util.Arrays;
 public class c221e{
   public static void main(String[] args) {
-    char[][] output;
-    String[] wordsIn;
+    char[][] output; //2d array to form grid for characters
+    String[] wordsIn; // array formed from input
     Scanner in = new Scanner(System.in);
     System.out.println("Welcome to the Word Snakifier. Please enter your words.");
     System.out.println("Please bear in mind that the word snake must consist of words that chain together by the last and first letters, seperated by spaces e.g. essence erect together -> essencerectogether");
@@ -12,59 +12,59 @@ public class c221e{
     input=input.trim();
     input=input.toUpperCase();
     int size=input.length();
-    output=new char[size][size];
-    for (char[] row:output){
+    output=new char[size][size]; //initialise output array
+    for (char[] row:output){//fills empty grid with dots for spacing
       Arrays.fill(row,'.');
     }
-    wordsIn=input.split(" ");
-    char[] currWord;
-    int x=0;
-    int y=0;
-    int dir=0; //0=right,1=up,2=down
-    Random rand = new Random();
+    wordsIn=input.split(" ");//forms input array
+    char[] currWord; //current word being operated on as a character array
+    int x=0;//x position in array/on grid
+    int y=0; //y position in array/on grid
+    int dir=0; //defines direction words will form on grid in, 0=right,1=up,2=down
+    Random rand = new Random(); //instantiates random number generator
     for (String item : wordsIn){
-      System.out.println(item);
+      //System.out.println(item);
       currWord=item.toCharArray();
-      for (char letter : currWord){
+      /*for (char letter : currWord){
         System.out.println(letter);
-      }
-      if (dir==0){
+      }*/
+      if (dir==0){ //forms word left to right
         for (char letter: currWord){
           output[y][x]=letter;
           x++;
         }
-        x--;
-        if (y<item.length()){dir=2;}
+        x--; //takes position on grid back to that of last letter; letters must "chain" as part of challenge
+        if (y<item.length()){dir=2;} //collision detection with top edge of grid;
         else{
           while(dir==0){
-            dir=rand.nextInt(2);
+            dir=rand.nextInt(2); //randomises next word's direction as up or down
           }
         }
       }
-      else if (dir==1){
+      else if (dir==1){ //forms word upwards
         for (char letter:currWord){
           output[y][x]=letter;
           y--;
         }
-        y++;
-        dir=0;
+        y++; //takes position back as above
+        dir=0; // sets next word direction to right to avoid collision
       }
-      else if (dir==2){
+      else if (dir==2){ //forms word downwards
         for (char letter:currWord){
           output[y][x]=letter;
           y++;
         }
-        y--;
-        dir=0;
+        y--; //takes position back as above
+        dir=0;//sets next word direction to right to avoid collision
       }
     }
-    String printString="";
+    String printString=""; //prints grid row by row as a string
     for (char[] row : output){
       for (char letter : row){
         printString=printString+String.valueOf(letter);
       }
       System.out.println(printString);
-      printString="";
+      printString=""; //resets string
     }
   }
 }
