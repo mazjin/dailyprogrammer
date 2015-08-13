59a1a0aab144e34e39d63b00d01d3069ef24560d
mazjin <mazjin55555@gmail.com>
2015-08-13 12:56:22 +0100

    modified code to be more modular. TODO: rewrite to use sxs grid of numbers instead of 2s-1 x 2s-1 grid with empty gaps, modify print function to include arrow-dashes instead

--- c227e.java
+++ c227e.java
@@ -9,16 +9,48 @@ public class c227e{
       System.out.println("Enter spiral size S");
     }while(!in.hasNextInt());
     s=in.nextInt();
-    grid=new char[(2*s)-1][(2*s)-1];
-    for (char[] row : grid){
-      Arrays.fill(row,' ');
+    if (s%2==0){
+      s++;
+      System.out.println("S needs to be an odd number, setting to "+s+" instead");
     }
-    for (int j=0;j>=(2*s)-1;j=j+2){
-      for (int i=0;i>=(2*s)-1;i=i+2){
-        grid[j][i]='+';
+    grid=new char[(2*s)-1][(2*s)-1];
+    grid=fillGrid(grid,s);
+    grid[s][s]='X';
+    printGrid(grid);
+  }
+
+
+  public static char[][] fillGrid(char[][] grid, int s){
+    int count=0;
+    for (char[] row : grid){
+      count=0;
+      //Arrays.fill(row,' ');
+      for (char letter : row){
+        System.out.println("col " + count%2);
+        if (count%2==0){
+          letter='+';
+        }
+        else{
+          letter=' ';
+        }
+        count++;
       }
     }
-    grid[s][s]='X';
+    /*for (int j=0;j>=(2*s)-1;j++){
+      for (int i=0;i>=(2*s)-1;i++){
+        if ((i%2==0)&&(j%2==0)){
+          System.out.println("BLONK");
+          grid[j][i]='+';
+        }
+      }
+    }*/
+    return grid;
+  }
+
+
+
+  public static void printGrid(char[][] grid){
+
     String outputLine="";
     for (char[] row : grid){
       for (char letter : row){
