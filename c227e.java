5a95336272e825bb7cb55ad285ce28e85bf3e822
mazjin <mazjin55555@gmail.com>
2015-08-13 17:09:23 +0100

    [C227e] convert relevant values to long, still experiencing OutOfMemoryError

--- c227e.java
+++ c227e.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 import java.util.Arrays;
 public class c227e{
   public static void main(String[] args) {
-    int[][] grid;
+    long[][] grid;
     Scanner in = new Scanner(System.in);
     int s;
     String searchString="";
-    int searchInt=0;
+    long searchLong=0;
     do{
       System.out.println("Enter spiral size S");
     }while(!in.hasNextInt());
@@ -35,18 +35,17 @@ public class c227e{
       System.out.println("coords");
       */searchString=in.next();
     //}
-    grid=new int[s][s];
+    grid=new long[s][s];
     grid=fillGrid(grid,s,spiralDirection);
     printGrid(grid);
     if (searchString.matches(".*\\D+.*")){
       System.out.println("coords");
       searchString=searchString.trim();
-      //searchString=searchString.substring(1,-1);
       System.out.println(searchString);
-      String[] searchInts=searchString.split("\\D");
-      int x=Integer.parseInt(searchInts[0]);
-      int y=Integer.parseInt(searchInts[1]);
-      System.out.println("search coords are "+x+","+y);//+","+searchInts[1]);
+      String[] searchCoords=searchString.split("\\D");
+      int x=Integer.parseInt(searchCoords[0]);
+      int y=Integer.parseInt(searchCoords[1]);
+      System.out.println("search coords are "+x+","+y);
       System.out.println(findInSpiral(grid,s,x,y));
     }
     else if (searchString==""){
@@ -54,20 +53,20 @@ public class c227e{
     }
     else{
       System.out.println("integer");
-      searchInt=Integer.parseInt(searchString.trim());
-      System.out.println("search integer is "+searchInt);
-      System.out.println(findInSpiral(grid,s,searchInt));
+      searchLong=Long.parseLong(searchString.trim());
+      System.out.println("search integer is "+searchLong);
+      System.out.println(findInSpiral(grid,s,searchLong));
     }
   }
 
 
-  public static int[][] fillGrid(int[][] grid, int s, int spiralDirection){
-    for (int[] row : grid){
+  public static long[][] fillGrid(long[][] grid, int s, int spiralDirection){
+    for (long[] row : grid){
       Arrays.fill(row,0);
     }
     int i=(s-1)/2;
     int j=(s-1)/2;
-    int count=1;
+    long count=1;
     int dir=0;
     int armLength=1;
     int armDistance=0;
@@ -102,14 +101,14 @@ public class c227e{
     return grid;
   }
 
-  public static String findInSpiral(int[][] grid, int s, int searchInt){
+  public static String findInSpiral(long[][] grid, long s, long searchLong){
     int xPos=0;
     int yPos=0;
       while (yPos<=(s-1)){
         while (xPos<=(s-1)){
           //System.out.println(xPos+","+yPos+"="+grid[yPos][xPos]);
-          if (grid[yPos][xPos]==searchInt){
-            return ("Found "+searchInt+" at position ("+(xPos+1)+","+(yPos+1)+")");
+          if (grid[yPos][xPos]==searchLong){
+            return ("Found "+searchLong+" at position ("+(xPos+1)+","+(yPos+1)+")");
           }
           xPos++;
         }
@@ -117,17 +116,17 @@ public class c227e{
         yPos++;
       }
 
-    return ("Search did not find " + searchInt + " in the spiral. Sorry!");
+    return ("Search did not find " + searchLong + " in the spiral. Sorry!");
   }
-  public static String findInSpiral(int[][] grid, int s, int xPos,int yPos){
-      int searchValue=grid[yPos-1][xPos-1];
+  public static String findInSpiral(long[][] grid, long s, int xPos,int yPos){
+      long searchValue=grid[yPos-1][xPos-1];
       return ("Found "+searchValue+" at position ("+(xPos)+","+(yPos)+")");
   }
-  public static void printGrid(int[][] grid){
+  public static void printGrid(long[][] grid){
 
     String outputLine="";
-    for (int[] row : grid){
-      for (int value : row){
+    for (long[] row : grid){
+      for (long value : row){
         if (value<10){
           outputLine=outputLine+0+value+" ";
         }
