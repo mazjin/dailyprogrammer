859e3a4cc5f01ac1ea8a7da65e0260de72ef357d
mazjin <mazjin55555@gmail.com>
2015-09-03 19:48:53 +0100

    [C209e] fix reading info into arrays, add flair processing

--- c209e.java
+++ c209e.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class c209e{
   public static void main(String[] args) throws IOException{
     int[] flairs;
-    String file="c09efile.txt";
+    String file="c209efile.txt";
     if (args.length==0){
       Scanner in= new Scanner(System.in);
       do {
@@ -27,42 +27,46 @@ public class c209e{
     double tempTime=0d;
     int i=0;
     int linecount=1;
-    boolean empty=true;
+    double prev=0;
+    for (double tim : times){
+      tim=315569660d;
+    }
     do {
       readIn=read.readLine();
       System.out.println(readIn);
       if (readIn==null){break;}
       name=readIn.substring(0,readIn.indexOf(":")).trim();
-      System.out.println(name);
       time=Double.parseDouble(readIn.substring(readIn.indexOf(":")+1).trim());
-      System.out.println(time);
-      i=0;
-      for (double tim : times){
-        tim=315569660d;
-      }
+      i=numOfUsers-1;
       do {
-        if (times[i]>time){
+        if (times[i]<time){
           tempTime=times[i];
           tempName=names[i];
           times[i]=time;
           names[i]=name;
           time=tempTime;
           name=tempName;
-          empty=false;
         }
-        i++;
-      } while (i<numOfUsers);
-      if (empty){
-        times[0]=time;
-        names[0]=name;
-        empty=false;
-      }
-      System.out.println(empty);
+        i--;
+      } while (i>=0);
       linecount++;
     } while (linecount<=numOfUsers);
     i=0;
     do {
       System.out.println(names[i]+" at "+times[i]);
+      i++;
+    } while (i<numOfUsers);
+    i=0;
+    do {
+      tempTime=(int)60-(times[i]-prev);
+      System.out.println(tempTime);
+      prev=times[i];
+      times[i]=tempTime;
+      i++;
+    } while (i<numOfUsers);
+    i=0;
+    do {
+      System.out.println(names[i]+" got flair "+(int)times[i]);
       i++;
     } while (i<numOfUsers);
   }
